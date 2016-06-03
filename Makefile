14dd3cf1566db5c2313d96873b8ffbc893cf9082
oleg <afa@cnwork.techranch.local>
2016-06-03 13:49:18 +0300

    init

---
+++ Makefile
@@ -0,0 +1,5 @@
+PROJECT = erl_swot
+PROJECT_DESCRIPTION = New project
+PROJECT_VERSION = 0.0.1
+
+include erlang.mk
---
+++ src/erl_swot_app.erl
@@ -0,0 +1,11 @@
+-module(erl_swot_app).
+-behaviour(application).
+
+-export([start/2]).
+-export([stop/1]).
+
+start(_Type, _Args) ->
+	erl_swot_sup:start_link().
+
+stop(_State) ->
+	ok.
